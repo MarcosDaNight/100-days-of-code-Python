f30ba0af50893812851096bc8afe50d3754a500e
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2022-04-25 10:36:07 -0300

    day-18 turtle-challenge 3 drawn shape

--- day-18/drawing-the-dots/steps/turtle_challenges.py
+++ day-18/drawing-the-dots/steps/turtle_challenges.py
@@ -1,22 +1,27 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 tim.shape("turtle")
 tim.color("yellow green")
 
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue",
+           "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
 
 # TODO challenge 1
 def make_square():
-    '''Function for turtle make a square'''
+    """Function for turtle make a square"""
     for _ in range(4):
         tim.fd(100)
         tim.rt(90)
 
 
-#make_square()
+# make_square()
 
-#TODO make a dashed line
+# TODO make a dashed line
 def make_drashed_line():
+    """Function for turtle make a dashed line"""
     for _ in range(15):
         tim.fd(10)
         tim.up()
@@ -24,7 +29,23 @@ def make_drashed_line():
         tim.down()
 
 
-make_drashed_line()
+# make_drashed_line()
+
+# TODO make a any gemoetric figure with turtle
+def drawn_shape(sides):
+    """Fucntion to make a any gemoetric figure"""
+    angle = 360 / sides
+
+    for _ in range(sides):
+        tim.fd(100)
+        tim.rt(angle)
+
 
 screen = Screen()
+screen.delay(40)
+
+for shape_side_n in range(3, 10):
+    tim.color(random.choice(colours))
+    drawn_shape(shape_side_n)
+
 screen.exitonclick()
