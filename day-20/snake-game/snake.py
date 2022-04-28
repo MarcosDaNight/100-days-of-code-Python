cc4fc3f82db9f6322a00ef90cf4f482d5d46712d
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2022-04-28 17:54:16 -0300

    day-20 functio for move using arrows done

--- day-20/snake-game/snake.py
+++ day-20/snake-game/snake.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 
 
 class Snake:
@@ -9,6 +13,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -24,4 +29,24 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
 
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.seth(RIGHT)
+            self.move()
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.seth(LEFT)
+            self.move()
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.seth(UP)
+            self.move()
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.seth(DOWN)
+            self.move()
