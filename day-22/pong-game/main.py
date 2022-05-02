93badca7e636019e92b0e92c3878eb51603d578e
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2022-05-02 08:37:01 -0300

    day-22 paddle class complete

--- day-22/pong-game/main.py
+++ day-22/pong-game/main.py
@@ -10,12 +10,15 @@ screen.bgcolor("black")
 screen.title("Pong Game.py")
 screen.tracer(0)
 
-paddle = Paddle()
+paddle_left = Paddle((-350, 0))
+paddle_right = Paddle((350, 0))
 scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(paddle.up, "Up")
-screen.onkey(paddle.down, "Down")
+screen.onkey(paddle_right.up, "Up")
+screen.onkey(paddle_right.down, "Down")
+screen.onkey(paddle_left.up, "w")
+screen.onkey(paddle_left.down, "s")
 
 game_is_on = True
 while game_is_on:
--- day-22/pong-game/paddle.py
+++ day-22/pong-game/paddle.py
@@ -1,45 +1,26 @@
 from turtle import Turtle
 
-STARTING_POSITIONS = [(-350, 60), (-350, 40), (-350, 20), (-350, 0), (-350, -20), (-350, -40)]
+STRETCH_WID = 5
+STRETCH_LEN = 1
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
-RIGHT = 0
-LEFT = 180
 
 
-class Paddle:
+class Paddle(Turtle):
 
-    def __init__(self):
-        self.segments = []
-        self.create_paddle()
-        self.head = self.segments[0]
-
-    def create_paddle(self):
-        for position in STARTING_POSITIONS:
-            self.add_segment(position)
-
-    def add_segment(self, position):
-        new_segment = Turtle(shape="square")
-        new_segment.color("white")
-        new_segment.penup()
-        new_segment.goto(position)
-        self.segments.append(new_segment)
-
-    def move(self):
-        for seg_num in range(len(self.segments) - 1, 0, -1):
-            new_x = self.segments[seg_num - 1].xcor()
-            new_y = self.segments[seg_num - 1].ycor()
-            self.segments[seg_num].goto(new_x, new_y)
-
-        self.head.forward(MOVE_DISTANCE)
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.goto(position)
+        self.shapesize(stretch_wid=STRETCH_WID, stretch_len=STRETCH_LEN)
 
     def up(self):
-        if self.head.heading() != DOWN:
-            self.head.seth(UP)
-            self.move()
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
 
     def down(self):
-        if self.head.heading() != UP:
-            self.head.seth(DOWN)
-            self.move()
+        new_y = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
