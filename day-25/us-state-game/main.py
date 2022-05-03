08be15edce5301ab84b0e6749384c5e1cf9e5f88
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2022-05-03 15:21:32 -0300

    day-25 DataFrames & Series

--- day-25/us-state-game/main.py
+++ day-25/us-state-game/main.py
@@ -1,20 +1,29 @@
-# with open('weather_data.csv') as data_file:
-#     data = data_file.readlines()
-#     print(data)
-
-# import csv
-#
-# with open("weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     print(data)
-#     for row in data:
-#         if not row[1] == 'temp':
-#             temperatures.append(int(row[1]))
-#
-#     print(temperatures)
-
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
 print(data["temp"])
+
+data_list = data["temp"].to_list()
+print(data_list)
+
+average = (sum(data_list)) // len(data_list)
+pandas_average = data["temp"].mean()
+max_temperature = data["temp"].max()
+
+print(f"The temperature average is {average}\nThe highest temperature is {max_temperature}")
+
+# Get Data in Columns
+print(data["condition"])
+print(data.condition)
+
+# Get data in Row
+print(data[data.day == "Monday"])
+
+# Create a DataFrame from scratch
+
+data_dict = {
+    "students": ["Amy", "Max", "Liza"],
+    "scores": [56, 89, 92]
+}
+dataFrame = pandas.DataFrame(data_dict)
+dataFrame.to_csv("new_data.csv")
