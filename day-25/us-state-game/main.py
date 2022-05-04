6d2712a979e5abdb61bc82bbe6ab9bdbdc6770f8
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2022-05-04 08:54:39 -0300

    day-25 creating data census

--- day-25/us-state-game/main.py
+++ day-25/us-state-game/main.py
@@ -1,29 +1,49 @@
 import pandas
+#
+# data = pandas.read_csv("weather_data.csv")
+# print(data["temp"])
+#
+# data_list = data["temp"].to_list()
+# print(data_list)
+#
+# average = (sum(data_list)) // len(data_list)
+# pandas_average = data["temp"].mean()
+# max_temperature = data["temp"].max()
+#
+# print(f"The temperature average is {average}\nThe highest temperature is {max_temperature}")
+#
+# # Get Data in Columns
+# print(data["condition"])
+# print(data.condition)
+#
+# # Get data in Row
+# print(data[data.day == "Monday"])
+#
+# # Create a DataFrame from scratch
+#
+# data_dict = {
+#     "students": ["Amy", "Max", "Liza"],
+#     "scores": [56, 89, 92]
+# }
+# dataFrame = pandas.DataFrame(data_dict)
+# dataFrame.to_csv("new_data.csv")
 
-data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+data_frame = pandas.read_csv("2018-squirrel_count.csv")
+gray = 0
+cinnamon = 0
+black = 0
 
-data_list = data["temp"].to_list()
-print(data_list)
+for color in data_frame["Primary Fur Color"]:
+    if color == "Gray":
+        gray += 1
+    elif color == "Cinnamon":
+        cinnamon += 1
+    elif color == "Black":
+        black += 1
 
-average = (sum(data_list)) // len(data_list)
-pandas_average = data["temp"].mean()
-max_temperature = data["temp"].max()
-
-print(f"The temperature average is {average}\nThe highest temperature is {max_temperature}")
-
-# Get Data in Columns
-print(data["condition"])
-print(data.condition)
-
-# Get data in Row
-print(data[data.day == "Monday"])
-
-# Create a DataFrame from scratch
-
-data_dict = {
-    "students": ["Amy", "Max", "Liza"],
-    "scores": [56, 89, 92]
+squirrel_dict = {
+    "Four Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray, cinnamon, black]
 }
-dataFrame = pandas.DataFrame(data_dict)
-dataFrame.to_csv("new_data.csv")
+squirrel_data_frame = pandas.DataFrame(squirrel_dict)
+squirrel_data_frame.to_csv("squirrel_count.csv")
