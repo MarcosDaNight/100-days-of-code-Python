a92ed7d2b3c3aeec9d3901e3478d2945de86eebb
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2022-05-05 20:34:25 -0300

    day-25 saving missing states in .csv data

--- day-25/us-state-game/main.py
+++ day-25/us-state-game/main.py
@@ -10,8 +10,6 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-
-
 data = pandas.read_csv("50_states.csv")
 states = data["state"].tolist()
 
@@ -20,7 +18,12 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
-    print(answer_state)
+    if answer_state == "Exit":
+        missing_states = []
+        for state in states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        break
 
     if answer_state in states:
         guessed_states.append(answer_state)
@@ -30,5 +33,8 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
+
+missing_data_frame = pandas.DataFrame(missing_states)
+missing_data_frame.to_csv("missing_states.csv")
 
 screen.exitonclick()
