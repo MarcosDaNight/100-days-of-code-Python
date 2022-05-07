f69ebc5ae7e6052f679ac42b55c36a76d75445cc
Marcos Guillermo <53832367+MarcosDaNight@users.noreply.github.com>
2022-05-07 16:16:36 -0300

    Update main.py

--- day-32/birthday-wisher/main.py
+++ day-32/birthday-wisher/main.py
@@ -39,10 +39,3 @@ if today_tuple in birthdays_dict:
             connection.login(user=MY_EMAIL, password=MY_PASSWORD)
             connection.send_message(msg)
             connection.quit()
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name
-# from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
