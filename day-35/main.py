428057c60385f7cc35b9f30ba9ff3e51e0948a6d
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2022-05-20 17:43:31 -0300

    day-35 challenge - check it will rain

--- day-35/main.py
+++ day-35/main.py
@@ -3,10 +3,13 @@ import requests
 OWM_EndPoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = "7bf5efa44bc739f3a32824ccc5e04d0d"  # your API key
 
+rain_lat = 51.626640
+rain_lon = 15.761730
+
 parameters = {
     "lat": -6.907790,
     "lon": -37.521290,
-    "exclude": "hourly",
+    "exclude": "current,minutely,daily",
     "appid": api_key
 }
 
@@ -14,10 +17,25 @@ parameters = {
 def get_weather():
     response = requests.get(url=OWM_EndPoint, params=parameters)
     response.raise_for_status()
-    print(response.status_code)
-    print(response)
-    data = response.json()
-    return data
+    return response.json()
 
 
-print(get_weather())
+def verify_rain(data_weather):
+    weather_status = data_weather['hourly'][0]['weather'][0]['id']
+    return weather_status < 700
+
+
+data = get_weather()
+weather_slice = data['hourly'][:12]
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data['weather'][0]['id']
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Take your umbrella!")
+
+else:
+    print("Right now, the time is ok!")
