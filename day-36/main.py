8ba71e8db63253d4b7e0d9647bc16040f2035e00
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2022-05-24 22:28:46 -0300

    day-36 get the news articles

--- day-36/main.py
+++ day-36/main.py
@@ -7,6 +7,7 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "319TWXQHDGPV1ESC"
+NEWS_API_KEY = "7e2c856c85434800ad187b03d6313733"
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -18,17 +19,27 @@ response = requests.get(STOCK_ENDPOINT, params=stock_params)
 data = response.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in data.items()]
 yesterday_data = data_list[0]
-yesterday_closing_price = yesterday_data["4. close"]
+yesterday_closing_price = float(yesterday_data["4. close"])
 
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-
 diff_percent = (difference / yesterday_closing_price) * 100
 
-if diff_percent > 5:
-    print("Get News")
+#if diff_percent > 5: print("Get News")
+
+if diff_percent > 1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+
+    three_articles = articles[:3]
+    print(three_articles)
 
 # Optional: Format the SMS message like this:
 """
