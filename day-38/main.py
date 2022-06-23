408cf362a0a2503541e70cc9185d1f7c34f48119
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2022-06-23 09:11:41 -0300

    step 02 get exercise stats with natural language

--- day-38/main.py
+++ day-38/main.py
@@ -1,2 +1,30 @@
-API_ID = "f759806b"
+import requests
+
+GENDER = "MALE"
+WEIGHT_KG = 60
+HEIGHT_CM = 180
+AGE = 22
+
+APP_ID = "f759806b"
 API_KEY = "9976cda182cb7970d08ce5295503f4af"
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me witch exercise you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
